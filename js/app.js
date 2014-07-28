72a86cd2ada34b0343e04326774c5335d70fa5ce
vikrambabu <babu@Dominics-MacBook-Air.local>
2014-07-27 21:56:47 -0700

    bugfixes

--- js/app.js
+++ js/app.js
@@ -141,9 +141,9 @@ window.onload = function() {
           },
           h4: {
             fz: val[1] + 'em',
-            mt: line/val[1] + 'em',
-            lh: line/val[1] + 'em',
-            mb: line/val[1] + 'em'
+            mt: line/val[1]/2 + 'em',
+            lh: line/val[1]/2 + 'em',
+            mb: line/val[1]/2 + 'em'
           },
           h5: {
             fz: 1 + 'em',
